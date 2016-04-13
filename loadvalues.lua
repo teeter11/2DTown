8069c330b6b33088ffc5416a757f3cc9945961ce
Teeter11 <teeter11teeter11@gmail.com>
2016-04-13 00:34:06 -0700

    Revert "Revert "Update 1""
    This reverts commit 621518283c27f25490b33a774b6e367ef835c57e.

---
+++ loadvalues.lua
@@ -0,0 +1,4 @@
+
+function LoadValues()
+
+end
--- main.lua
+++ main.lua
@@ -2,21 +2,36 @@
 function love.load()
 
 	require("loadcontent")
+	require("input")
+	require("loadvalues")
 	
+	LoadValues()
 	LoadContent()
-
-
+	
+	windowWidth = love.graphics.getWidth()
+	windowHeight = love.graphics.getHeight()
+	
+	gameState = "mainmenu" 
+	
 end
 
 function love.draw()
-
+	
+	if gameState = "mainmenu" then
+	
+	elseif gameState = "ingame" then
+	
+	end
+	
 end
 
 function love.update()
 
+	
+	Input()
 end
 
 function love.exit()
-
+	
 end
 
